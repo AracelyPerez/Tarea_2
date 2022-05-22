55ef0b76129ac039ecac53f0e5118724ca32088d
Aracely1607 <aracely.perez97@gmail.com>
2022-05-21 22:37:14 -0400

    Update main.c

--- .vscode/main.c
+++ .vscode/main.c
@@ -1,1 +1,18 @@
 // hola hola.
+
+#include <unistd.h>
+#include <ncurses.h>
+
+int main(int argc, char *argv[]){
+	int i;
+	initscr();
+	erase();
+	for(i=0;i<20;i++){
+		move(3,i);
+		printw(" x");
+		refresh();
+		sleep(1);
+    }
+	endwin();
+  return(0);
+}
